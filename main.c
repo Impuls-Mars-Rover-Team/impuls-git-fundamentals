680fe2b0353a18c53dd3208461583622d3178cdd
gwitkowski2000 <gwitkowski2000@gmail.com>
2023-09-21 11:01:19 +0200

    hello gwitkowski2000

---
+++ main.c
@@ -0,0 +1,14 @@
+/*------------ Hello section -----------*/
+/*
+ * @gwitkowski2000: hello impuls
+ */
+
+#include <stm32f407xx.h>
+#include <stm32f4xx.h>
+
+int main(void){
+	
+	while(1){
+		__NOP();
+	}
+}
